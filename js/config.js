cfd5c9498db96f8ce0f823d8aea4f089f5c08699
Jordan Robey <jrobey72@gmail.com>
2023-06-25 14:09:59 -0400

    api config change

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-var apiUrl = 'http://127.0.0.1:5000/'; // development
-// var apiUrl = 'https://tpocc4w4nj.execute-api.us-east-1.amazonaws.com/prod/'; // prod
+// var apiUrl = 'http://127.0.0.1:5000/'; // development
+var apiUrl = 'https://tpocc4w4nj.execute-api.us-east-1.amazonaws.com/prod/'; // prod
